80726f82949a720e3218b9836ee8b23979c80f71
AnatOWJIya <43040730+AnatOWJIya@users.noreply.github.com>
2019-02-03 15:15:05 +0300

    Create maks_1.py

---
+++ maks_1.py
@@ -0,0 +1,16 @@
+a = [int (x) for x in input("Enter  number sequence:\n").split()]
+dict = {}
+big = 1
+for i in a:
+    if i not in dict:
+        dict[i] = 1
+    else:
+        dict[i] +=1
+dict1=dict.copy()
+for k in dict:
+    if dict.get(k) > big:
+        big = dict.get(k)
+    else:
+        dict1.pop(k)
+print(dict)
+print(dict1)
